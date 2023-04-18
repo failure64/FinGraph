10514b7434338e8152612703172fc4870cf0224a
failure64 <78782223+failure64@users.noreply.github.com>
2023-04-19 00:06:45 +0530

    Add files via upload

---
+++ client/src/expanded-theme.ts
@@ -0,0 +1,12 @@
+import createPalette from "@mui/material/styles/createPalette";
+import { Palette, PaletteColor } from "@mui/material/styles/createPalette";
+
+declare module "@mui/material/styles/createPalette"{
+    interface PaletteColor{
+        [key: number]: string;
+    }
+
+    interface Palette{
+        tertiary: PaletteColor;
+    }
+}
